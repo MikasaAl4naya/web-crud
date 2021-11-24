73d30f09118ab686f7dace6c89b13b5ddd200f82
MikasaAl4naya <oleg.pytin@gmail.com>
2021-11-24 18:56:09 +0800

    Закончил вторую

--- framework/TwigBaseController.php
+++ framework/TwigBaseController.php
@@ -2,40 +2,44 @@
 require_once "BaseController.php";
 
 class TwigBaseController extends BaseController {
-    public $title = ""; 
-    public $template = "";
-    public $info = "";
-    public $description = "";
-    public $objectID = "";
-    public $menu = [];
-    protected \Twig\Environment $twig; 
+    public $title = ""; // название страницы
+    public $template = ""; // шаблон страницы
+    protected \Twig\Environment $twig;
+    public $url ="";
+    public $menu = [
+        [
+            "title" => "Главная",
+            "url" => "/",
+        ],
+        [
+            "title" => "Slipknot",
+            "url" => "/Slipknot",
     
-      public function setTwig($twig) {
+    
+        ],
+        [
+            "title" => "LinkinPark",
+            "url" => "/LinkinPark",
+    
+        ]
+    ];
+    
+    public function setTwig($twig) {
         $this->twig = $twig;
     }
 
     
+    
     public function getContext() : array
     {
-        $context = parent::getContext();
-        $context['title'] = $this->title; 
-        $query = $this->pdo->prepare("SELECT title, id FROM bands");
-        $query->execute(); 
-        $data = $query->fetchAll();
-        $context['menu'] = [];
-        for ($i = 0; $i <= count($data) - 1; $i++){
-
-            array_push( $context['menu'], [
-                    "title" => $data[$i]['title'],
-                    "url" => "/bands/".$data[$i]['id'],
-            ]);
-
-        }
-        $url = $_SERVER["REQUEST_URI"];
-        $context['url'] = $url;
+        $context = parent::getContext(); // вызываем родительский метод
+        $context['title'] = $this->title; // добавляем title в контекст
+        $context['menu'] = $this->menu;
+        $context['url'] = $this->url;
 
         return $context;
     }
+    
     
     public function get() {
         echo $this->twig->render($this->template, $this->getContext());
